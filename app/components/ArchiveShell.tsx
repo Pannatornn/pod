611a0f5da17455e37993034f2f58cc5f359521a7
Pannatornn <pannatorn252@gmail.com>
2026-09-15 17:21:57 +0700

    Delete app directory

--- app/components/ArchiveShell.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { ReactNode } from 'react';
-
-export default function ArchiveShell({children}:{children:ReactNode}){
- return <div className="archive-shell">{children}<div className="archive-scanline" /></div>;
-}
